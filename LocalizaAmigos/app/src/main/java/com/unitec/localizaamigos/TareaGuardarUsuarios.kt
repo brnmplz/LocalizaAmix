2fd9fe408d53073e2e52863dc0a0c4bba9920d23
brunoplaza <bruno.a.mplaza@gmail.com>
2020-11-12 15:34:46 -0600

    Work

---
+++ LocalizaAmigos/app/src/main/java/com/unitec/localizaamigos/TareaGuardarUsuarios.kt
@@ -0,0 +1,16 @@
+package com.unitec.localizaamigos
+
+import android.os.AsyncTask
+
+class TareaGuardarUsuarios: AsyncTask<Void,Void,Void>(){
+    override fun onPreExcecute(){
+        super.onPreExcute()
+    }
+
+    override fun doInBackground(vararg p0: Void?): Void{
+        TODO("not yet implemented")
+    }
+    override fun onPostExecute(result: Void){
+        super.onPostExecute(result)
+    }
+}
